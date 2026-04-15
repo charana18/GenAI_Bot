911de6997552eb09a3966327e143cf331e901540
charana18 <charanashree4@gmail.com>
2026-04-15 10:55:42 +0530

    Update App.py

--- App.py
+++ App.py
@@ -4,3 +4,28 @@ from groq import Groq
 st.set_page_config("pragyanAI Content Genrator", layout = "wide")
 st.title("pragyanAI- Content Generator")
 st.image("nature.jpg")
+
+client = Groq (api_key=st.secrets["groq_api_key"])
+product = st.text_input("Product")
+audience = st.text_input("Audience")
+# Button to Generate Content
+if st.button("Generate Content"):
+    prompt = f"Write marketing content for {product} targeting {audience}."
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    st.session_state.text = response.choices[0].message.content
+    text =response.choices[0].message.content
+    st.write(text)
+# After Content Create - Download The File
+if "text" in st.session_state:
+    content = st.text_area("Generated Content", st.session_state.text, height=300)
+    st.download_button(
+            label="⬇️ Download as TXT",
+            data=content,
+            file_name="marketing_copy.txt",
+            mime="text/plain"
+        )
+    else:
+        st.info("Generate content first")
